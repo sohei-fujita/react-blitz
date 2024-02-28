b561ec9688ee46566b311fa2785a434554d2217b
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-02-28 21:21:41 +0900

    react start

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement);
+
+const App = () => {
+    return  <h1>こんにちは！</h1>
+}
+
+root.render
+    (<StrictMode>
+        <App />
+    </StrictMode>
+);
